55dd523ab66ff520a2f87a19ddf854b7090b6c2b
Denis Vershadov <@denis.vershadov>
2023-03-29 10:54:23 +0300

    fix mobile details view

--- school-ratings/pages/components/Filters/Filters.jsx
+++ school-ratings/pages/components/Filters/Filters.jsx
@@ -42,89 +42,91 @@ const Filters = ({filters, setFilters}) => {
             {isFiltersOpen && (
                 <div className={styles.filters}>
                     <CloseIcon className={styles.filters__icon_close} onClick={handleOpenFilters}/>
-                    <ul className={styles.filters__list}>
-                        <span className={styles.filters__list_title}>
-                            Mieszkanie
-                        </span>
-                        <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'poor')}>
-                            <Image
-                                className={getImageStyle(flat.poor)}
-                                src='/png/home-icon-blue.png' width={20} height={32} alt='mieszkanie'/>
-                            <span>2</span>
-                        </li>
-                        <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'fair')}>
-                            <Image className={getImageStyle(flat.fair)} src='/png/home-icon-green.png'
-                                   width={20}
-                                   height={32} alt='mieszkanie'/>
-                            <span>3</span>
-                        </li>
-                        <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'average')}>
-                            <Image className={getImageStyle(flat.average)} src='/png/home-icon-yellow.png'
-                                   width={20} height={32} alt='mieszkanie'/>
-                            <span>4</span>
-                        </li>
-                        <li className={styles.filters__list_item} onClick={handleClickGood}>
-                            <Image className={getImageStyle(flat.good)} src='/png/home-icon-red.png'
-                                   width={20} height={32}
-                                   alt='mieszkanie'/>
-                            <span>5+</span>
-                        </li>
-                    </ul>
-                    <ul className={styles.filters__list}>
-                        <span className={styles.filters__list_title}>
-                            Mieszkanie nie aktualne
-                        </span>
-                        <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'unavailable')}>
-                            <Image className={getImageStyle(flat.unavailable)} src='/png/home-icon-gray.png'
-                                   width={20} height={32} alt='mieszkaniee nie aktualne'/>
-                        </li>
-                    </ul>
-                    <hr className={styles.filters__hr}/>
-                    <ul className={styles.filters__list}>
-                        <span className={styles.filters__list_title}>
-                            Szkoła Podstawowa
-                        </span>
-                        <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'fair')}>
-                            <Image className={getImageStyle(school.fair)} src='/png/school-icon-blue.png'
-                                   width={20} height={32} alt='szkoła podstawowa'/>
-                            <span>2</span>
-                        </li>
-                        <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'average')}>
-                            <Image className={getImageStyle(school.average)} src='/png/school-icon-green.png'
-                                   width={20} height={32} alt='szkoła podstawowa'/>
-                            <span>3</span>
-                        </li>
-                        <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'good')}>
-                            <Image className={getImageStyle(school.good)} src='/png/school-icon-yellow.png'
-                                   width={20} height={32} alt='szkoła podstawowa'/>
-                            <span>4</span>
-                        </li>
-                        <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'excellent')}>
-                            <Image className={getImageStyle(school.excellent)} src='/png/school-icon-red.png'
-                                   width={20} height={32} alt='szkoła podstawowa'/>
-                            <span>5</span>
-                        </li>
-                    </ul>
-                    <ul className={styles.filters__list}>
-                        <span className={styles.filters__list_title}>
-                            Przedszkole Prywatne
-                        </span>
-                        <li className={styles.filters__list_item} onClick={handleClickPreschool.bind(null, 'private')}>
-                            <Image className={getImageStyle(preschool.private)}
-                                   src='/png/preschool-icon-private.png' width={20} height={32}
-                                   alt='prywatne przedszkole'/>
-                        </li>
-                    </ul>
-                    <ul className={styles.filters__list}>
-                        <span className={styles.filters__list_title}>
-                            Przedszkole Publiczne
-                        </span>
-                        <li className={styles.filters__list_item} onClick={handleClickPreschool.bind(null, 'public')}>
-                            <Image className={getImageStyle(preschool.public)}
-                                   src='/png/preschool-icon-public.png' width={20} height={32}
-                                   alt='przedszkole publiczne'/>
-                        </li>
-                    </ul>
+                    <div className={styles.filters__container}>
+                        <ul className={styles.filters__list}>
+                            <span className={styles.filters__list_title}>
+                                Mieszkanie
+                            </span>
+                            <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'poor')}>
+                                <Image
+                                    className={getImageStyle(flat.poor)}
+                                    src='/png/home-icon-blue.png' width={20} height={32} alt='mieszkanie'/>
+                                <span>2</span>
+                            </li>
+                            <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'fair')}>
+                                <Image className={getImageStyle(flat.fair)} src='/png/home-icon-green.png'
+                                       width={20}
+                                       height={32} alt='mieszkanie'/>
+                                <span>3</span>
+                            </li>
+                            <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'average')}>
+                                <Image className={getImageStyle(flat.average)} src='/png/home-icon-yellow.png'
+                                       width={20} height={32} alt='mieszkanie'/>
+                                <span>4</span>
+                            </li>
+                            <li className={styles.filters__list_item} onClick={handleClickGood}>
+                                <Image className={getImageStyle(flat.good)} src='/png/home-icon-red.png'
+                                       width={20} height={32}
+                                       alt='mieszkanie'/>
+                                <span>5+</span>
+                            </li>
+                        </ul>
+                        <ul className={styles.filters__list}>
+                            <span className={styles.filters__list_title}>
+                                Mieszkanie nie aktualne
+                            </span>
+                            <li className={styles.filters__list_item} onClick={handleClickFlat.bind(null, 'unavailable')}>
+                                <Image className={getImageStyle(flat.unavailable)} src='/png/home-icon-gray.png'
+                                       width={20} height={32} alt='mieszkaniee nie aktualne'/>
+                            </li>
+                        </ul>
+                        <hr className={styles.filters__hr}/>
+                        <ul className={styles.filters__list}>
+                            <span className={styles.filters__list_title}>
+                                Szkoła Podstawowa
+                            </span>
+                            <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'fair')}>
+                                <Image className={getImageStyle(school.fair)} src='/png/school-icon-blue.png'
+                                       width={20} height={32} alt='szkoła podstawowa'/>
+                                <span>2</span>
+                            </li>
+                            <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'average')}>
+                                <Image className={getImageStyle(school.average)} src='/png/school-icon-green.png'
+                                       width={20} height={32} alt='szkoła podstawowa'/>
+                                <span>3</span>
+                            </li>
+                            <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'good')}>
+                                <Image className={getImageStyle(school.good)} src='/png/school-icon-yellow.png'
+                                       width={20} height={32} alt='szkoła podstawowa'/>
+                                <span>4</span>
+                            </li>
+                            <li className={styles.filters__list_item} onClick={handleClickSchool.bind(null, 'excellent')}>
+                                <Image className={getImageStyle(school.excellent)} src='/png/school-icon-red.png'
+                                       width={20} height={32} alt='szkoła podstawowa'/>
+                                <span>5</span>
+                            </li>
+                        </ul>
+                        <ul className={styles.filters__list}>
+                            <span className={styles.filters__list_title}>
+                                Przedszkole Prywatne
+                            </span>
+                            <li className={styles.filters__list_item} onClick={handleClickPreschool.bind(null, 'private')}>
+                                <Image className={getImageStyle(preschool.private)}
+                                       src='/png/preschool-icon-private.png' width={20} height={32}
+                                       alt='prywatne przedszkole'/>
+                            </li>
+                        </ul>
+                        <ul className={styles.filters__list}>
+                            <span className={styles.filters__list_title}>
+                                Przedszkole Publiczne
+                            </span>
+                            <li className={styles.filters__list_item} onClick={handleClickPreschool.bind(null, 'public')}>
+                                <Image className={getImageStyle(preschool.public)}
+                                       src='/png/preschool-icon-public.png' width={20} height={32}
+                                       alt='przedszkole publiczne'/>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             )}
         </>
--- school-ratings/pages/components/List/List.jsx
+++ school-ratings/pages/components/List/List.jsx
@@ -35,41 +35,42 @@ const List = ({flats, setCenter, setFlatDetail}) => {
             {isListOpen && (
                 <div className={styles.list}>
                     <CloseIcon className={styles.list__icon_close} onClick={handleOpenList}/>
-                    <ul className={styles.list__list}>
-                        {flats ? flats.map((flat) => {
-                            const {id, rating, price, phone, address, link, location, available} = flat;
+                    <div className={styles.list__container}>
+                        <ul className={styles.list__list}>
+                            {flats ? flats.map((flat) => {
+                                const {id, rating, price, phone, address, link, location, available} = flat;
 
-                                return (
-                                    <li key={id} className={styles.list__list_item}>
-                                        {location.lat && location.lng
-                                            ? (<Image className={styles.list__list_item_img}
-                                                      src={getIcon(rating, available)}
-                                                      width={20} height={32} alt='geolokalizacja'
-                                                      onClick={handleSetCenter.bind(null, location)}/>)
-                                            : (<span className={styles.list__list_item_no_img}></span>)}
-                                        {price && (<span className={styles.list__list_item__price}>{price}zł</span>)}
-                                        {phone && (
-                                            <span className={styles.list__list_item__phone}>
-                                                <PhoneIcon className={styles.list__icon__phone} alt='telefon'/>
-                                                <span>{phone}</span>
-                                            </span>
-                                        )}
-                                        {address && (<span>{address}</span>)}
-                                        {link && (
-                                            <a className={styles.list__list_item__link} target="_blank" href={link}>
-                                                <LinkIcon className={styles.list__icon__link} alt='link'/>
-                                            </a>
-                                        )}
-                                        <EditIcon className={styles.list__icon__edit} alt='redagować'
-                                                  onClick={handleEdit.bind(null, flat)}/>
-                                    </li>
-                                )
-                            }
-                        ) : (
-                            <li className={styles.list__list_item_empty}>Pusta lista...</li>
-                        )}
-                    </ul>
-
+                                    return (
+                                        <li key={id} className={styles.list__list_item}>
+                                            {location.lat && location.lng
+                                                ? (<Image className={styles.list__list_item_img}
+                                                          src={getIcon(rating, available)}
+                                                          width={20} height={32} alt='geolokalizacja'
+                                                          onClick={handleSetCenter.bind(null, location)}/>)
+                                                : (<span className={styles.list__list_item_no_img}></span>)}
+                                            {price && (<span className={styles.list__list_item__price}>{price}zł</span>)}
+                                            {phone && (
+                                                <span className={styles.list__list_item__phone}>
+                                                    <PhoneIcon className={styles.list__icon__phone} alt='telefon'/>
+                                                    <span>{phone}</span>
+                                                </span>
+                                            )}
+                                            {address && (<span>{address}</span>)}
+                                            {link && (
+                                                <a className={styles.list__list_item__link} target="_blank" href={link}>
+                                                    <LinkIcon className={styles.list__icon__link} alt='link'/>
+                                                </a>
+                                            )}
+                                            <EditIcon className={styles.list__icon__edit} alt='redagować'
+                                                      onClick={handleEdit.bind(null, flat)}/>
+                                        </li>
+                                    )
+                                }
+                            ) : (
+                                <li className={styles.list__list_item_empty}>Pusta lista...</li>
+                            )}
+                        </ul>
+                    </div>
                 </div>
             )}
         </>
